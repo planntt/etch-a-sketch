1c199fc06892b5a7fb606461b8d0e017f15ae85b
Mads <madi.gillis@gmail.com>
2022-08-01 16:35:41 -0700

    Dunno what I changed here oops

--- js-file.js
+++ js-file.js
@@ -2,6 +2,6 @@ const container = document.querySelector("#container");
 
 let htmlString = ""
 for (let i = 0; i < 16; i++) {
-    htmlString += `<div></div>`;
+    htmlString += `<div>${i}</div>`;
 };
 container.innerHTML = htmlString;
